ea0d1d1a61a683b61cfaa1a84b62c7d48b75b885
ananyyavijay <ananyavijay05@gmail.com>
2025-07-22 23:12:56 +0530

    added localstorage

---
+++ index.js
@@ -0,0 +1,4 @@
+import TodoForm from './todoForm';
+import TodoList from './todoList';
+
+export { TodoForm, TodoList };
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoProvider } from "./context";
 
 function App() {
@@ -19,8 +19,26 @@ function App() {
   };
 
   const toggleTodo = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo));
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
   };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <TodoProvider
